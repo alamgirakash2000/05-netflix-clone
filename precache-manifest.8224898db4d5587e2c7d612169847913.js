2d7a7c916a2bb738943c3e8227d7da0f7ba5da24
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-08-09 11:38:06 +0600

    Updates

---
+++ precache-manifest.8224898db4d5587e2c7d612169847913.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796718e9717d22304dae6198c10c2f85",
+    "url": "/05-netflix-clone/index.html"
+  },
+  {
+    "revision": "a8671932bb9077ac1e1e",
+    "url": "/05-netflix-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5aeae8f71f9b02a21881",
+    "url": "/05-netflix-clone/static/js/2.4d3d95d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/05-netflix-clone/static/js/2.4d3d95d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8671932bb9077ac1e1e",
+    "url": "/05-netflix-clone/static/js/main.5e9b39d7.chunk.js"
+  },
+  {
+    "revision": "fb1d4f99bb28560624a0",
+    "url": "/05-netflix-clone/static/js/runtime-main.895b1f08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/05-netflix-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
